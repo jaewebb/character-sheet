178a960ea9dbb4a1a2d52922f4e363072380916a
Jae Webb <jynnae.webb@gmail.com>
2024-11-12 16:17:23 -0500

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,94 +1,135 @@
 import Image from "next/image";
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid2';
 import styles from "./page.module.css";
+
+interface AbilityScore {
+  base: string
+  modifier: string
+  name: string
+}
+
+interface Skill {
+  bonus: string
+  expertise: boolean
+  modifier: string
+  name: string
+  proficiency: boolean
+}
+
+function Character() {
+  return (
+    <Grid container spacing={2}>
+      <Grid size={1}>
+        <Image
+          src="/avatar.png"
+          alt="Avatar for character Jae Webb"
+          width={75}
+          height={74}
+          priority
+        />
+      </Grid>
+      <Grid size={3}>
+        <Box>Jae Webb</Box>
+        <Box>Human Senior Web Application Developer</Box>
+        <Box>Level 20</Box>
+      </Grid>
+    </Grid>
+  )
+}
+
+function AbilityScores() {
+  const abilityScores = [
+    { base: '20', modifier: '+5', name: 'Strength' },
+    { base: '20', modifier: '+5', name: 'Dexterity' },
+    { base: '20', modifier: '+5', name: 'Constitution' },
+    { base: '20', modifier: '+5', name: 'Intelligence' },
+    { base: '20', modifier: '+5', name: 'Wisdom' },
+    { base: '20', modifier: '+5', name: 'Charisma' }
+  ]
+
+  return (
+    abilityScores.map((score: AbilityScore) => {
+      return (
+        <Grid key={score.name} size={1} sx={{ border: 1, borderRadius: 2, textAlign: 'center', paddingY: 2}}>
+          <Box sx={{ fontSize: '14px' }}>{score.name}</Box>
+          <Box sx={{ fontSize: 'h4.fontSize' }}>{score.modifier}</Box>
+          <Box sx={{ fontSize: 'h6.fontSize' }}>{score.base}</Box>
+        </Grid>
+      )
+    })
+  )
+}
+
+function Skills() {
+  const skills = [
+    { bonus: '+15', expertise: true, modifier: '', name: 'TypeScript', proficiency: true },
+    { bonus: '+15', expertise: true, modifier: '', name: 'JavaScript', proficiency: true },
+    { bonus: '+15', expertise: true, modifier: '', name: 'HTML5', proficiency: true },
+    { bonus: '+15', expertise: true, modifier: '', name: 'CSS3', proficiency: true },
+    { bonus: '+15', expertise: true, modifier: '', name: 'Vue.js', proficiency: true },
+    { bonus: '+15', expertise: true, modifier: '', name: 'Pinia', proficiency: true },
+    { bonus: '+10', expertise: false, modifier: '', name: 'React', proficiency: true },
+    { bonus: '+10', expertise: false, modifier: '', name: 'Vuex', proficiency: true },
+    { bonus: '+10', expertise: false, modifier: '', name: 'Vue Router', proficiency: true },
+    { bonus: '+10', expertise: false, modifier: '', name: 'Node.js', proficiency: true },
+    { bonus: '+10', expertise: false, modifier: '', name: 'npm', proficiency: true },
+    { bonus: '+10', expertise: false, modifier: '', name: 'Git', proficiency: true },
+    { bonus: '+10', expertise: false, modifier: '', name: 'REST APIs', proficiency: true },
+    { bonus: '+10', expertise: false, modifier: '', name: 'Accessibility', proficiency: true },
+    { bonus: '+10', expertise: false, modifier: '', name: 'Vercel', proficiency: true },
+    { bonus: '+10', expertise: false, modifier: '', name: 'SQL', proficiency: true },
+    { bonus: '+10', expertise: false, modifier: '', name: 'Vitest', proficiency: true },
+    { bonus: '+10', expertise: false, modifier: '', name: 'Axe', proficiency: true },
+    { bonus: '+5', expertise: false, modifier: '', name: 'Material UI', proficiency: false },
+  ]
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Prof</th>
+          <th>Skill</th>
+          <th>Bonus</th>
+        </tr>
+      </thead>
+      <tbody>
+        {skills.map((skill: Skill) => {
+          return (
+            <tr key={skill.name}>
+              <td>
+                <Box sx={{ textAlign: 'center' }}>{skill.expertise ? '◉' : skill.proficiency ? '●' : '○' }</Box>
+              </td>
+              <td>
+                <Box>{skill.name}</Box>
+              </td>
+              <td>
+                <Box sx={{ border: 1, borderRadius: 2, fontSize: 'h6.fontSize', textAlign: 'center' }}>{skill.bonus}</Box>
+              </td>
+            </tr>
+          )})}
+      </tbody>
+    </table>
+  )
+}
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
+        Coming Soon!
+        {/* <Character />
+        <Grid container spacing={2}>
+          <AbilityScores />
+        </Grid>
+        <Grid container spacing={2}>
+          <Grid sx={{ paddingY: 2, textAlign: 'left'}} size={4}>
+            <Skills />
+          </Grid>
+        </Grid> */}
       </main>
       <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+        
       </footer>
     </div>
   );
